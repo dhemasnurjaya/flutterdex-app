d52adb0b2d2bd699b1edd424ae3c8bc0aabe880d
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2024-04-27 21:58:15 +0700

    sprite source update (wip)

--- lib/presentation/pokemon_list/pokemon_sprite.dart
+++ lib/presentation/pokemon_list/pokemon_sprite.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutterdex/injection_container.dart';
@@ -16,13 +17,23 @@ class PokemonSprite extends StatelessWidget {
     final spritesPath = 'assets/sprites/pokemon/$pokemonId.png';
     final isSpriteAvailable =
         getIt<AssetManifest>().listAssets().contains(spritesPath);
-
-    return FadeInImage(
-      placeholder: Image.asset(placeholderSpritePath).image,
+    final localImage = FadeInImage(
       fit: BoxFit.cover,
+      placeholder: Image.asset(placeholderSpritePath).image,
       image: Image.asset(
         isSpriteAvailable ? spritesPath : placeholderSpritePath,
       ).image,
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: CachedNetworkImage(
+        fit: BoxFit.cover,
+        placeholder: (_, __) => Image.asset(placeholderSpritePath),
+        imageUrl:
+            'https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${pokemonId.toString().padLeft(3, '0')}.png',
+        errorWidget: (_, __, ___) => localImage,
+      ),
     );
   }
 }
